1a0156d511cee88832aea96f97b53bbd4b1ced4c
HaVTHE172621 <117827609+VuTrongHa@users.noreply.github.com>
2024-05-22 20:26:57 +0700

    update logo(2)

--- Source Code/online-learning/src/Assets/homePageForLectures/nav-bar/Navbar.js
+++ Source Code/online-learning/src/Assets/homePageForLectures/nav-bar/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Navbar.css';  // Optional: Create a separate CSS file for Navbar styles
 import '../nav-bar/Navbar.css';
+import logo from '../imgs/logo (1).jpg';
 
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="logo">
-        <img src="/logo.png" alt="TOAD Learning" className="logo-img" />
+        <img src={logo} alt="Logo" className="logo-img" />
       </div>
       <input type="text" placeholder="Search..." className="search-bar" />
       <div className="nav-buttons">
