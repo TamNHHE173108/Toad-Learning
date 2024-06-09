5cd80fe6cf088a8bceb069c8e442b63a88ef144b
DangPHHE172554 <dangphhe172554@fpt.edu.vn>
2024-06-09 22:08:51 +0700

    Update ListCourse.java

--- Source Code/Toad-Learning/src/java/controller/ListCourse.java
+++ Source Code/Toad-Learning/src/java/controller/ListCourse.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package controller;
 
 import dao.CourseDAO;
@@ -19,28 +18,31 @@ import java.util.List;
  *
  * @author laptop lenovo
  */
-@WebServlet(name="ListCourse", urlPatterns={"/listcourse"})
+@WebServlet(name = "ListCourse", urlPatterns = {"/listcourse"})
 public class ListCourse extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         CourseDAO dao = new CourseDAO();
         List<Course> listC = dao.listCourses();
         request.setAttribute("listCourse", listC);
         request.getRequestDispatcher("/views/Hungpt/ListCourse.jsp").forward(request, response);
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -48,12 +50,16 @@ public class ListCourse extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        processRequest(request, response);
-    } 
+            throws ServletException, IOException {
+        CourseDAO courseDAO = new CourseDAO();
+        List<Course> courseList = courseDAO.listCourses();
+        request.setAttribute("listCourse", courseList);
+        request.getRequestDispatcher("/view/Tamnh/index.jsp").forward(request, response);
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -61,12 +67,13 @@ public class ListCourse extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /** 
+    /**
      * Returns a short description of the servlet.
+     *
      * @return a String containing servlet description
      */
     @Override
