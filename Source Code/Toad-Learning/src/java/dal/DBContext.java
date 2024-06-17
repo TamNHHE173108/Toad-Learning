dac97aed0399c5445cebc5f8dc2081005c3e2d50
Tamnhhe <tamnhhe@gmail.com>
2024-06-17 16:35:13 +0700

    Update DBContext.java

--- Source Code/Toad-Learning/src/java/dal/DBContext.java
+++ Source Code/Toad-Learning/src/java/dal/DBContext.java
@@ -1,4 +1,5 @@
 package dal;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,31 +10,24 @@ import java.util.logging.Logger;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author FPT University - PRJ30X
  */
-
-
-
 public class DBContext {
+
     protected Connection connection;
 
     public DBContext() {
-        // You can modify these variables to match your system configuration.
         String user = "sa";
         String pass = "123";
         String url = "jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=ToadLearning";
-        
+
         try {
-                
+
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-            
-           
             connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException | SQLException ex) {
-         
             Logger.getLogger(DBContext.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
@@ -42,4 +36,3 @@ public class DBContext {
         return connection;
     }
 }
-
