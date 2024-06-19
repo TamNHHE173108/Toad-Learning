fb2f9538b967f394496542117ab3b72f629ad625
HungPTHE176412 <HungPTHE176412@fpt.edu.vn>
2024-06-19 16:24:43 +0700

    Update DeleteUserDAO.java

--- Source Code/Toad-Learning/src/java/dao/DeleteUserDAO.java
+++ Source Code/Toad-Learning/src/java/dao/DeleteUserDAO.java
@@ -14,15 +14,48 @@ import java.sql.ResultSet;
  * @author laptop lenovo
  */
 public class DeleteUserDAO {
+
     Connection connection = null;
     PreparedStatement ps = null;
     ResultSet rs = null;
+
     public void deleteUser(String user_id) {
-        String query = "DELETE FROM Users WHERE UserID = ?";
+        String query = "DELETE FROM Scores\n"
+                + "WHERE UserID = ?\n"
+                + "DELETE FROM Histories\n"
+                + "WHERE UserID = ?\n"
+                + "DELETE FROM Registrations\n"
+                + "WHERE UserID = ?\n"
+                + "DELETE FROM Blogs\n"
+                + "WHERE UserID = ?\n"
+                + "DELETE FROM UserGroups\n"
+                + "WHERE UserID = ?\n"
+                + "DELETE FROM Questions\n"
+                + "WHERE QuizID IN (SELECT QuizID FROM Quizzes WHERE CourseID IN (SELECT CourseID FROM Courses WHERE UserID = ?));\n"
+                + "DELETE FROM Quizzes\n"
+                + "WHERE CourseID IN (SELECT CourseID FROM Courses WHERE UserID = ?);\n"
+                + "DELETE FROM Lessons\n"
+                + "WHERE CourseID IN (SELECT CourseID FROM Courses WHERE UserID = ?);\n"
+                + "DELETE FROM Media\n"
+                + "WHERE LessonID IN (SELECT LessonID FROM Lessons WHERE CourseID IN (SELECT CourseID FROM Courses WHERE UserID = ?));\n"
+                + "DELETE FROM Courses\n"
+                + "WHERE UserID = ?\n"
+                + "DELETE FROM Users\n"
+                + "WHERE UserID = ?";
         try {
             connection = new DBContext().getConnection();//mo ket noi voi sql
             ps = connection.prepareStatement(query);
             ps.setString(1, user_id);
+            ps.setString(2, user_id);
+            ps.setString(3, user_id);
+            ps.setString(4, user_id);
+            ps.setString(5, user_id);
+            ps.setString(6, user_id);
+            ps.setString(7, user_id);
+            ps.setString(8, user_id);
+            ps.setString(9, user_id);
+            ps.setString(10, user_id);
+            ps.setString(11, user_id);
             ps.executeUpdate();
         } catch (Exception e) {
         }
