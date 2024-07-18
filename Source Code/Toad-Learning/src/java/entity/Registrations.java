d0f8eab63e44f974d6dfd9569b7318669f601c3a
HaVTHE172621 <117827609+VuTrongHa@users.noreply.github.com>
2024-07-18 13:40:48 +0700

    update

--- Source Code/Toad-Learning/src/java/entity/Registrations.java
+++ Source Code/Toad-Learning/src/java/entity/Registrations.java
@@ -9,26 +9,45 @@ package entity;
  * @author Admin
  */
 public class Registrations {
-    private String userID;
+    private User userID;
     private String courseID;
-    
+    private String totalCost;
+    private String status;
+    private String validFrom;
+    private String validTO;
+    private String updateBy;
+    private String registrationTime;
+    private String scoreID;
+    private String score;
+    private String quizStatus;
 
     public Registrations() {
     }
 
-    public Registrations(String userID, String courseID) {
-        this.userID = userID;
+    public Registrations(User userID, String courseID, String totalCost, String status, String validFrom, String validTO, String updateBy, String registrationTime, String scoreID, String score, String quizStatus) {
+        this.userID = userID;   
         this.courseID = courseID;
+        this.totalCost = totalCost;
+        this.status = status;
+        this.validFrom = validFrom;
+        this.validTO = validTO;
+        this.updateBy = updateBy;
+        this.registrationTime = registrationTime;
+        this.scoreID = scoreID;
+        this.score = score;
+        this.quizStatus = quizStatus;
     }
 
-    public String getUserID() {
+    public User getUserID() {
         return userID;
     }
 
-    public void setUserID(String userID) {
+    public void setUserID(User userID) {
         this.userID = userID;
     }
 
+    
+    
     public String getCourseID() {
         return courseID;
     }
@@ -36,14 +55,83 @@ public class Registrations {
     public void setCourseID(String courseID) {
         this.courseID = courseID;
     }
-    
 
-    
+    public String getTotalCost() {
+        return totalCost;
+    }
 
-   
+    public void setTotalCost(String totalCost) {
+        this.totalCost = totalCost;
+    }
 
-    
-    
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getValidFrom() {
+        return validFrom;
+    }
+
+    public void setValidFrom(String validFrom) {
+        this.validFrom = validFrom;
+    }
+
+    public String getValidTO() {
+        return validTO;
+    }
+
+    public void setValidTO(String validTO) {
+        this.validTO = validTO;
+    }
+
+    public String getUpdateBy() {
+        return updateBy;
+    }
+
+    public void setUpdateBy(String updateBy) {
+        this.updateBy = updateBy;
+    }
+
+    public String getRegistrationTime() {
+        return registrationTime;
+    }
+
+    public void setRegistrationTime(String registrationTime) {
+        this.registrationTime = registrationTime;
+    }
+
+    public String getScoreID() {
+        return scoreID;
+    }
+
+    public void setScoreID(String scoreID) {
+        this.scoreID = scoreID;
+    }
+
+    public String getScore() {
+        return score;
+    }
+
+    public void setScore(String score) {
+        this.score = score;
+    }
+
+    public String getQuizStatus() {
+        return quizStatus;
+    }
+
+    public void setQuizStatus(String quizStatus) {
+        this.quizStatus = quizStatus;
+    }
+
+    @Override
+    public String toString() {
+        return "Registrations{" + "userID=" + userID + ", courseID=" + courseID + ", totalCost=" + totalCost + ", status=" + status + ", validFrom=" + validFrom + ", validTO=" + validTO + ", updateBy=" + updateBy + ", registrationTime=" + registrationTime + ", scoreID=" + scoreID + ", score=" + score + ", quizStatus=" + quizStatus + '}';
+    }
     
     
 }
